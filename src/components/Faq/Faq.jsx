e29a86a55e85fa1ac89fa2d2a45391b32f3f5094
IVKrylova <ivk.lis@yandex.ru>
2023-03-17 21:50:42 +0300

    feat: set up opening of the answer

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import './Faq.scss';
 
 const Faq = (props) => {
+  const [isAnswerOpened, setIsAnswerOpened] = useState(false);
+  const summeryClass = `faq__question ${isAnswerOpened ? 'faq__question_opened' : ''}`;
+
+  const handleClickQuestion = (evt) => {
+    evt.preventDefault();
+
+    if (!evt.target.closest('.faq__item').open) {
+      setIsAnswerOpened(true);
+    } else {
+      setIsAnswerOpened(false);
+    }
+  }
+
   return (
     <li className='faq'>
-      <details className='faq__item'>
-        <summary className='faq__question'>
+      <details className='faq__item' open={isAnswerOpened}>
+        <summary
+          className={summeryClass}
+          onClick={handleClickQuestion}
+          id={props.faq.id}
+        >
           {props.faq.question}
         </summary>
         <p className='faq__answer'>
