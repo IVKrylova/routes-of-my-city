cfd27c0a7e50e3a47e9d2b982010fd33e3328e4a
IVKrylova <ivk.lis@yandex.ru>
2023-03-22 10:17:59 +0300

    feat: add html and css code of the table

--- src/components/ResultsTable/ResultsTable.jsx
+++ src/components/ResultsTable/ResultsTable.jsx
@@ -11,7 +11,30 @@ const ResultsTable = (props) => {
         <li className='results-table__category'>Велопрофи</li>
       </ul>
       <table className='results-table__table'>
-
+        <thead className='results-table__table-head'>
+          <tr>
+            <th className='results-table__col-number'>№</th>
+            <th className='results-table__col-team'>Команда</th>
+            <th className='results-table__col-score'>Баллы</th>
+            <th className='results-table__col-time'>Время</th>
+          </tr>
+        </thead>
+        <tbody className='results-table__table-body'>
+          {props.result &&
+            props.result.map(el => {
+              return (
+                <tr key={el.id} className='results-table__row'>
+                  <th className='results-table__col-number results-table__col-number_body'>{el.number}</th>
+                  <th className='results-table__col-team'>
+                    <span className='results-table__col-team-body'>{el.team}</span>
+                  </th>
+                  <th className='results-table__col-score'>{el.score}</th>
+                  <th className='results-table__col-time'>{el.time}</th>
+                </tr>
+              );
+            })
+          }
+        </tbody>
       </table>
       <button className='results-table__button-more' type='button'>
         Показать ещё
