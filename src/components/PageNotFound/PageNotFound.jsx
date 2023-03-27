4a8af91fa9a2c9985417d561b7089b8e64988a2b
IVKrylova <ivk.lis@yandex.ru>
2023-03-27 10:56:04 +0300

    feat: add styles of the page not found

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,26 +1,28 @@
 import { Link } from 'react-router-dom';
-import iconPageNotFound from '../../images/icon-404.svg';
+import pageNotFound from '../../images/page-not-found.png';
 import './PageNotFound.scss';
 
 const PageNotFound = () => {
   return (
       <main className='page-not-found'>
         <img
-          src={iconPageNotFound}
+          src={pageNotFound}
           alt='иконка страница не найдена'
           className='page-not-found__img'
         />
-        <p className='page-not-found__title'>
-          Страница не найдена
-        </p>
-        <p className='page-not-found__message'>
-          Мы обязательно разберёмся с этим недоразумением,
-          а пока вернитесь на главную страницу
-        </p>
-        {/* ToDo: check link */}
-        <Link to='/' className='page-not-found__link-to-main'>
-          Вернуться на главную
-        </Link>
+        <div className='page-not-found__content'>
+          <p className='page-not-found__title'>
+            Страница не найдена
+          </p>
+          <p className='page-not-found__message'>
+            Мы обязательно разберёмся с этим недоразумением,
+            а пока вернитесь на главную страницу
+          </p>
+          {/* ToDo: check link */}
+          <Link to='/' className='page-not-found__link-to-main'>
+            Вернуться на главную
+          </Link>
+        </div>
       </main>
     );
   }
