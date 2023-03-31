8ad6c2b6c025cd12e61f34d9ed6d855f130a1596
IVKrylova <ivk.lis@yandex.ru>
2023-03-31 17:40:58 +0300

    refactor: code style

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,8 @@
 import Benefits from '../Benefits/Benefits';
 import AboutProject from '../AboutProject/AboutProject';
-
 import MainQuests from '../MainQuests/MainQuests';
 import FaqList from '../FaqList/FaqList';
 import MainBanner from '../MainBanner/MainBanner';
-
 
 import './Main.scss';
 const Main = (props) => {
