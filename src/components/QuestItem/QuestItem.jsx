f5d86b8c8282bb7c707606c164e6f0e5c76d5911
IVKrylova <ivk.lis@yandex.ru>
2023-04-03 21:33:43 +0300

    feat: add media queries

--- src/components/QuestItem/QuestItem.jsx
+++ src/components/QuestItem/QuestItem.jsx
@@ -9,12 +9,15 @@ const QuestItem = (props) => {
 
   const background = props.quest.isActive ?
     {
-      background: `radial-gradient(53.07% 53.07% at 33.71% 2.73%, rgba(1, 128, 162, 0.4) 0%, rgba(24, 123, 123, 0) 100%), linear-gradient(101.2deg, #003B59 15.99%, rgba(0, 59, 89, 0) 69.7%), 72% 30% url(${props.quest.img})`,
+      background: `radial-gradient(53.07% 53.07% at 33.71% 2.73%, rgba(1, 128, 162, 0.4) 0%, rgba(24, 123, 123, 0) 100%), linear-gradient(101.2deg, #003B59 15.99%, rgba(0, 59, 89, 0) 69.7%), 72% 30% url(${props.quest.img}) no-repeat`,
+      backgroundSize: 'cover',
     } :
     {
       background: `radial-gradient(44.43% 53.16% at 30.93% 5.57%, rgba(1, 128, 162, 0.2) 0%, rgba(1, 128, 162, 0) 100%), linear-gradient(102.92deg, #003B59 4.72%, rgba(0, 59, 89, 0) 65.23%), linear-gradient(0deg, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), 3% 100% url(${props.quest.img})`,
+      backgroundSize: 'cover',
     };
   const buttonClass = `quest-item__button-cta ${props.quest.isActive ? '' : 'quest-item__button-cta_disabled'} ${props.location.pathname === '/' ? '' : 'quest-item__button-cta_hidden'}`;
+  const cardClass =`quest-item__card ${props.location.pathname === '/' ? 'quest-item__card_place_home-page' : 'quest-item__card_place_quest-page'}`;
 
   const hendleButtonTakePart = () => {
     if (props.quest.isActive) props.sendQuestId(props.quest.id);
@@ -29,7 +32,7 @@ const QuestItem = (props) => {
 
   return (
     <div className='quest-item'>
-      <div className='quest-item__card' style={background}>
+      <div className={cardClass} style={background}>
         <h3 className='quest-item__title'>{props.quest.name}</h3>
         <p className='quest-item__description'>{props.quest.description}</p>
         <p className='quest-item__data'>{props.quest.date}</p>
