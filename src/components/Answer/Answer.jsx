ba3ca0c87d8d75f41935c555ec9231f27d9ad2dd
IVKrylova <ivk.lis@yandex.ru>
2023-04-06 16:15:33 +0300

    feat: add input type number

--- src/components/Answer/Answer.jsx
+++ src/components/Answer/Answer.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react';
 import './Answer.scss';
 
 const Answer = (props) => {
+  const [digits, setDigits] = useState([]);
+  const [currentInput, setCurrentInput] = useState(null);
+
+  // ToDo: fix with hook useFormAndValidation
+  const handleInputNumber = (evt) => {
+    const value = evt.target.value;
+    evt.target.value = value.replace(/\D/g, '');
+  }
+
+  useEffect(() => {
+    if (props.task.response.type === 'number') {
+      const arrDigits = [];
+      for (let i = 0; i < props.task.response.digits; i++) {
+        arrDigits[i] = i;
+      }
+      setDigits(arrDigits);
+    }
+  }, [props.task.response]);
+
   return (
     <form className='answer-form'>
       <label className='answer-form__answer' htmlFor={props.task.name}>
@@ -22,6 +42,27 @@ const Answer = (props) => {
             <span className='answer-form__input-validation'>
               {`${props.task.response.lengthStr} ${props.task.response.lengthStr === 1 ? 'символ' : (props.task.response.lengthStr > 4) ? 'символов' : 'символа'}`}
             </span>
+          </div>
+        }
+        {props.task.response.type === 'number' &&
+          <div className='answer-form__field-answer answer-form__field-answer_type_number'>
+            {digits && digits.map(el => {
+              return (
+                <input
+                  type='text'
+                  maxLength={1}
+                  minLength={1}
+                  pattern={/^[0-9]$/}
+                  required
+                  className='answer-form__input-number'
+                  id={props.task.name}
+                  name={props.task.name}
+                  onInput={handleInputNumber}
+                  key={el}
+                />
+              );
+            })}
+            <span className='answer-form__input-type'>Число</span>
           </div>
         }
       </label>
--- src/utils/data/task.js
+++ src/utils/data/task.js
@@ -7,14 +7,14 @@ export const taskItem = {
   img: imgTask,
   problem: `Думаю, для вас не секрет, где в городе производят самый популярный «алкогольный сувенир» Ставропольского края.\nНа другой стороне улицы вы сможете найти сказочный замок за стеклянным забором и необычное транспортное средство\nВнимательно исследуйте экспонат и запишите номер Элис.`,
   // response parameters, validation
-  response: {
+ /*  response: {
     type: 'text',
     lengthStr: 13,
-  },
-  /* response: {
+  }, */
+  response: {
     type: 'number',
-    exponent: 3,
-  } */
+    digits: 3,
+  }
   /* response: {
     type: 'date',
   } */
